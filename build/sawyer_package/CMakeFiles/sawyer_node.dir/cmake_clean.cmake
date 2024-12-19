dc64bb06ae05480f72c4c87b5156ae856b26701b
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2024-12-18 19:05:43 -0800

    Add files via upload

---
+++ build/sawyer_package/CMakeFiles/sawyer_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sawyer_node.dir/src/sawyer_node.cpp.o"
+  "CMakeFiles/sawyer_node.dir/src/sawyer_node.cpp.o.d"
+  "sawyer_node"
+  "sawyer_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sawyer_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/sawyer_package/CMakeFiles/sawyer_package_uninstall.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for sawyer_package_uninstall.
